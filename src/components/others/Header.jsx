7d83d770eebf54d336e6235d9049128d88443462
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-01 11:06:56 +0500

    feat: add header for employee dashboard

---
+++ src/components/others/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <div className="flex justify-between items-center p-4 border-b border-emerald-400/10">
+        <h1 className="leading-tight">
+          Hello,
+          <br />
+          <span className="text-2xl text-emerald-500">User</span>
+          👋
+        </h1>
+        <button className="cursor-pointer border border-white px-2.5 py-1 rounded-lg opacity-70 hover:text-red-400 hover:border-red-400 transition-colors">
+          Log Out
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Header;
