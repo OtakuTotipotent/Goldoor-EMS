6c7af87991e79a060786a031eb91af6631a0fcc2
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-10 22:04:12 +0500

    feat: add task cards to employee dashboard

--- src/components/Tasks/EmployeeTaskList.jsx
+++ src/components/Tasks/EmployeeTaskList.jsx
@@ -1,12 +1,18 @@
 import AcceptTask from "./AcceptTask";
+import NewTask from "./NewTask";
+import CompleteTask from "./CompleteTask";
+import FailedTask from "./FailedTask";
 
-export default function EmployeeTasksList({ data }) {
+export default function EmployeeTasksList() {
   return (
     <div
       id="taskList"
       className="p-2 m-5 mt-10 h-96 grid md:flex md:justify-between gap-1 overflow-x-auto scroll-smooth"
     >
-      <AcceptTask data={data} />
+      <AcceptTask />
+      <NewTask />
+      <CompleteTask />
+      <FailedTask />
     </div>
   );
 }
