4221efdbf97a838c4ef70b9ac14ad5f607cef868
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-12 09:28:52 +0500

    feat: add employee task list dynamically

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -3,16 +3,26 @@ import NewTask from "./NewTask";
 import CompleteTask from "./CompleteTask";
 import FailedTask from "./FailedTask";
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
   return (
     <div
       id="taskList"
       className="p-2 m-5 mt-10 h-96 grid md:flex md:justify-between gap-1 overflow-x-auto scroll-smooth"
     >
-      <AcceptTask />
-      <NewTask />
-      <CompleteTask />
-      <FailedTask />
+      {data.tasks.map((elem) => {
+        if (elem.active) {
+          return <AcceptTask />;
+        }
+        if (elem.newTask) {
+          return <NewTask />;
+        }
+        if (elem.completed) {
+          return <CompleteTask />;
+        }
+        if (elem.failed) {
+          return <FailedTask />;
+        }
+      })}
     </div>
   );
 };
