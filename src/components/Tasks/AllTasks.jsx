8f9fb2065666ace51dfbe35708569a3af27ad5e6
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-13 13:55:15 +0500

    fix: change heading for admin dashboard stats; add indexing for table data

--- src/components/Tasks/AllTasks.jsx
+++ src/components/Tasks/AllTasks.jsx
@@ -6,7 +6,7 @@ const AllTasks = () => {
 
   return (
     <div className="p-5 md:w-8/12 md:m-auto mt-8 mx-8 md:mt-15 px-10 md:px-0">
-      <h2 className="text-emerald-500 p-2">All Tasks</h2>
+      <h2 className="text-emerald-500 p-2">Employee Stats</h2>
       <div className="border-y border-emerald-500 rounded-sm py-5 px-1">
         <div className="grid grid-cols-5 bg-emerald-800 text-gray-300 rounded-sm text-md font-semibold items-center text-center p-2 cursor-default select-none mb-4 uppercase">
           <h5>Employee</h5>
@@ -16,9 +16,12 @@ const AllTasks = () => {
           <h5>Failed</h5>
         </div>
         <div className="flex flex-col gap-1.5 max-h-96 overflow-auto">
-          {authData.employees.map(function (employee) {
+          {authData.employees.map(function (employee, index) {
             return (
-              <div className="bg-emerald-500/20 rounded-lg p-4 grid grid-cols-5 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300 text-center">
+              <div
+                key={index}
+                className="bg-emerald-500/20 rounded-lg p-4 grid grid-cols-5 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300 text-center"
+              >
                 <h5 className="text-left">{employee.firstName}</h5>
                 <h5>{employee.taskCounts.newTask}</h5>
                 <h5>{employee.taskCounts.active}</h5>
