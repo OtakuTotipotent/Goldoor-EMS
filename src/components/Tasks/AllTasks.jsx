b276befb20a5b9fe2fbd85fad71c506b24ecaa2a
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-13 13:40:10 +0500

    feat: show employees with stats in admin dashboard

--- src/components/Tasks/AllTasks.jsx
+++ src/components/Tasks/AllTasks.jsx
@@ -1,28 +1,36 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
+
 const AllTasks = () => {
+  const authData = useContext(AuthContext);
+
   return (
     <div className="p-5 md:w-8/12 md:m-auto mt-8 mx-8 md:mt-15 px-10 md:px-0">
       <h2 className="text-emerald-500 p-2">All Tasks</h2>
       <div className="border-y border-emerald-500 rounded-sm py-5 px-1">
+        <div className="grid grid-cols-5 bg-emerald-800 text-gray-300 rounded-sm text-md font-semibold items-center text-center p-2 cursor-default select-none mb-4 uppercase">
+          <h5>Employee</h5>
+          <h5>New</h5>
+          <h5>Active</h5>
+          <h5>Completed</h5>
+          <h5>Failed</h5>
+        </div>
         <div className="flex flex-col gap-1.5 max-h-96 overflow-auto">
-          <div className="bg-emerald-500/20 rounded-lg p-4 flex justify-between gap-4 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300">
-            <h2>@username</h2>
-            <h3>"Task Title is here"</h3>
-            <h5>Pending</h5>
-          </div>
-          <div className="bg-emerald-500/20 rounded-lg p-4 flex justify-between gap-4 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300">
-            <h2>@username</h2>
-            <h3>"Task Title is here"</h3>
-            <h5>Pending</h5>
-          </div>
-          <div className="bg-emerald-500/20 rounded-lg p-4 flex justify-between gap-4 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300">
-            <h2>@username</h2>
-            <h3>"Task Title is here"</h3>
-            <h5>Pending</h5>
-          </div>
+          {authData.employees.map(function (employee) {
+            return (
+              <div className="bg-emerald-500/20 rounded-lg p-4 grid grid-cols-5 cursor-default hover:bg-emerald-500/30 transition-colors text-gray-300 text-center">
+                <h5 className="text-left">{employee.firstName}</h5>
+                <h5>{employee.taskCounts.newTask}</h5>
+                <h5>{employee.taskCounts.active}</h5>
+                <h5>{employee.taskCounts.completed}</h5>
+                <h5>{employee.taskCounts.failed}</h5>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 };
 
-export default AllTasks
+export default AllTasks;
